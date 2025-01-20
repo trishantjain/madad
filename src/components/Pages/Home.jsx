060ed145de1744f505a97e26c30b58ea937ad4ce
trishantjain <trishantjain8888@gmail.com>
2025-01-20 22:53:53 +0530

    Added 'Home' component

---
+++ src/components/Pages/Home.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+import '../css/main.css';
+import Options from '../Options';
+
+export default function Main() {
+    return (
+        <div className="main">
+            <div>
+                <h4>Select the options to get the Colleges</h4>
+            </div>
+
+            <Options />
+        </div>
+    )
+}
